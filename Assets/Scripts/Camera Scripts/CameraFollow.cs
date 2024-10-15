20d88705e455913900419b4e6fc77f8ea3d52425
May H <mayh500@gmail.com>
2024-10-15 22:22:52 +0300

    updated camera script

--- Assets/Scripts/Camera Scripts/CameraFollow.cs
+++ Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
-    public Transform target;
+    GameObject target;
+    public GameObject[] characterArray;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        FindActivePlayer();
+        Vector3 newPos = new Vector3(target.transform.position.x, target.transform.position.y, -10f);
         transform.position = Vector3.Slerp(transform.position,newPos,FollowSpeed*Time.deltaTime);
+    }
+
+    public void FindActivePlayer()
+    {
+        for(int i = 0; i < characterArray.Length; i++)
+        {
+            if (characterArray[i].activeSelf)
+            {
+                target = characterArray[i];
+            }
+        }
     }
 }
