c28d539217ba8cc222644178c37ee0bc78895386
May H <mayh500@gmail.com>
2024-10-22 21:56:48 +0300

    added scripts

---
+++ Assets/Scripts/ScriptableObjects/Floatvariable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class Floatvariable : ScriptableObject
+{
+    public float value;
+}
---
+++ Assets/Scripts/UI Scripts/ButtonScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public StringVariables outcome;
+    public GameObject popUPWindow;
+    public void RoundOutcome()
+    {
+        switch (outcome.value)
+        {
+            case "onGoing":
+
+                popUPWindow.SetActive(false);
+                Time.timeScale = 1f;
+
+                break;
+
+            case "gameEnd":
+                popUPWindow.SetActive(false);
+                SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+                SceneManager.LoadSceneAsync("TestScene");
+                break;
+
+        }
+    }
+}
