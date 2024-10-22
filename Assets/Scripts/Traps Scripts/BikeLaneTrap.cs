65d1a2e7b3627fd5dfb0d4ae84d79ee9c661e004
May H <mayh500@gmail.com>
2024-10-22 22:33:16 +0300

    updated scripts and added round system

--- Assets/Scripts/Traps Scripts/BikeLaneTrap.cs
+++ Assets/Scripts/Traps Scripts/BikeLaneTrap.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 public class BikeLaneTrap : ITraps
 {
     AudioManager audioManager;
+    public Floatvariable remainingTime;
     private void Awake()
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        FailToPassTrap(collision, audioManager, "Car", "Pedestrian");
+        FailToPassTrap(collision, audioManager, "Car", "Pedestrian", remainingTime);
     }
 
 }
