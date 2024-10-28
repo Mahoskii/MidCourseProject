8f2a32a68ad03eda5a46c5ca144b95541785c452
May H <mayh500@gmail.com>
2024-10-28 17:45:37 +0200

    .

---
+++ Assets/Scripts/ScriptableObjects/DeliveryPointScriptable.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DeliveryPointScriptable : ScriptableObject
+{
+public List<Vector2> DeliveryPointsLocationList = new List<Vector2> { 
+    new Vector2(4394, -116),
+    new Vector2(914, 1816), 
+    new Vector2(4738, 1135),
+    new Vector2(7288, 4025), 
+    new Vector2(-798, 4078)};
+}
