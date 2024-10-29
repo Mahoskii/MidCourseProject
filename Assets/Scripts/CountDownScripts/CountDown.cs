8e95b4a8268ed60a5c0767829ad454becee133f4
May H <mayh500@gmail.com>
2024-10-29 21:58:31 +0200

    more bugs!

--- Assets/Scripts/CountDownScripts/CountDown.cs
+++ Assets/Scripts/CountDownScripts/CountDown.cs
@@ -6,30 +6,37 @@ using UnityEngine;
 public class CountDown : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
-    public Floatvariable remainingTime;
+    public OneScriptToRuleThemAll roundTimeSupplier;
+    public float remainingTime = 0;
     [Header("Events")]
     public GameEvent onTimeOver;
     private bool isThereTime = true;
 
+    private void Start()
+    {
+        UpdateTimer();
+        roundTimeSupplier.timeIndex++;
+    }
+
     void Update()
     {
-        if (remainingTime.value > 0)
+        if (remainingTime > 0)
         {
-            remainingTime.value -= Time.deltaTime;
-            TimeDisplay(remainingTime.value);
-            if (remainingTime.value <= 0.01)
+            remainingTime -= Time.deltaTime;
+            TimeDisplay(remainingTime);
+            if (remainingTime <= 0.01)
             {
                 onTimeOver.Raise();
             }
 
         }
-        else if (remainingTime.value <= 0)
+        else if (remainingTime <= 0)
         {
-            remainingTime.value = 0;
-            TimeDisplay(remainingTime.value);
-            if (remainingTime.value <= 0 && isThereTime)
+            remainingTime = 0;
+            TimeDisplay(remainingTime);
+            if (remainingTime <= 0 && isThereTime)
             {
-                remainingTime.value += 0.2f;
+                remainingTime += 0.2f;
             }
         }
     }
@@ -39,5 +46,25 @@ public class CountDown : MonoBehaviour
         float minutes = Mathf.FloorToInt(remainingTime / 60);
         float seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void UpdateTimer()
+    {
+        remainingTime = roundTimeSupplier.RoundTimes[roundTimeSupplier.timeIndex];
+    }
+    public void UpdateCounter()
+    {
+        if (roundTimeSupplier.timeIndex < 4)
+        {
+            roundTimeSupplier.timeIndex++;
+        }
+    }
+    public void TurnSelfOff()
+    {
+        gameObject.SetActive(false);
+    }
+    public void ResetCounter()
+    {
+        roundTimeSupplier.timeIndex = 0;
     }
 }
